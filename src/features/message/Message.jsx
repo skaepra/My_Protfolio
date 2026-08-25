4950a61265197e8faa871a8594658ecaefc15f23
skaepra <fierdragon6@gmail.com>
2026-08-25 05:19:25 +0300

    fix

--- src/features/message/Message.jsx
+++ src/features/message/Message.jsx
@@ -25,8 +25,8 @@ function MessageScreen() {
    setMessage("");
   }
   return (
-    <div className="flex justify-center items-center min-h-screen dark:bg-zinc-900 px-4">
-      <div className="w-full max-w-md bg-[#eff2f3] dark:bg-zinc-800/60 backdrop-blur-sm p-1.5 sm:p-6 rounded-3xl space-y-4 border border-slate-300 dark:border-zinc-700/50 shadow-xl"> 
+    <div className="flex justify-center  dark:bg-zinc-900 p-4">
+      <div className="w-full max-w-md bg-[#eff2f3] dark:bg-zinc-800/60 backdrop-blur-sm p-6 rounded-3xl space-y-4 border border-slate-300 dark:border-zinc-700/50 shadow-xl"> 
         
         <h1 className="text-xl font-bold  text-center mb-2">Send Message</h1>
         
